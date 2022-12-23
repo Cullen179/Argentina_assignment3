61467f3eb21481dfa8c7792e8a041d6f894235e6
Ngoc <tranlehongngoc2017@gmail.com>
2022-12-22 21:09:27 -0600

    Customer: add sortProducts() method

--- src/Customer.java
+++ src/Customer.java
@@ -75,8 +75,8 @@ public class Customer {
 
             String[] product = line.split(",");
 
-            String category = product[1];
             Double price = Double.valueOf(product[2]);
+            String category = product[3];
 
             // if the search category matches the category and the price range => display the product(s)
             if (category.equals(searchCategory) && minimum < price && price < maximum) {
@@ -89,8 +89,74 @@ public class Customer {
             System.out.println("No matched category. Please try again!");
     }
 
-    public static void sortProduct() {
+    public static void sortProducts() throws IOException {
+        boolean correctInput = false;
+        int sortOrder;
 
+        // Notify user that only input 0 and 1 is available
+        do {
+            System.out.println("Choose the way you want to sort");
+            System.out.println("0.Ascending");
+            System.out.println("1.Descending");
+            Scanner sort = new Scanner(System.in);
+            sortOrder = sort.nextInt();
+            if (sortOrder == 0 || sortOrder == 1)
+                correctInput = true;
+            else
+                System.out.println("Wrong input! Enter 0 or 1 only. Please try again");
+        } while (!correctInput);
+
+        File file = new File("./src/File/products.txt");
+        Scanner productScanner = new Scanner(file);
+
+        // initialise an array list storing the products
+        List<Product> productsList = new ArrayList<>();
+
+        // Reading products.txt file and create new Product to put in Array List productsList
+        while (productScanner.hasNextLine()) {
+            String product = productScanner.nextLine();
+            String[] productInfo = product.split(",");
+            String productId = productInfo[0];
+            String productName = productInfo[1];
+            double productPrice = Double.parseDouble(productInfo[2]);
+            String productCategory = productInfo[3];
+
+            Product sortedProduct = new Product(productId, productName, productPrice, productCategory);
+            productsList.add(sortedProduct);
+
+            int finalSortOrder = sortOrder;
+
+            // Sort products inside the productsList array using Comparator
+            productsList.sort(new Comparator<Product>() {
+                @Override
+                public int compare(Product o1, Product o2) {
+                    if (finalSortOrder == 0) {
+                        if (o1.getPrice() == o2.getPrice())
+                            return 0;
+                        else if (o1.getPrice() > o2.getPrice())
+                            return 1;
+                        else
+                            return -1;
+                    } else {
+                        if (o1.getPrice() == o2.getPrice())
+                            return 0;
+                        else if (o1.getPrice() > o2.getPrice())
+                            return -1;
+                        else
+                            return 1;
+                    }
+                }
+            });
+        }
+
+        for (Product product : productsList) {
+            System.out.println("-----------------");
+            System.out.println("ID: " + product.getId());
+            System.out.println("Title: " + product.getName());
+            System.out.println("Price: " + product.getPrice());
+            System.out.println("Category: " + product.getCategory());
+            System.out.println("-----------------");
+        }
     }
     public static void createOrder() {
     }
