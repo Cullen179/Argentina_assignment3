950dde9df71024d866654052605f557728f05a35
Ngoc <tranlehongngoc2017@gmail.com>
2022-12-25 11:01:12 -0600

    Customer: create order method

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.time.LocalDate;
 import java.util.*;
 
 public class Customer {
@@ -16,6 +17,7 @@ public class Customer {
     public static boolean checkCustomerPasswordExisted(String password) {
 
     }
+
 
     public static void viewProducts() throws IOException {
         File file = new File("./src/File/products.txt");
@@ -158,7 +160,203 @@ public class Customer {
             System.out.println("-----------------");
         }
     }
-    public static void createOrder() {
+
+    // generating the order ID while still maintaining the format
+    public static String newOrderId() throws IOException {
+        Scanner sc = new Scanner(new File("src/orders.txt"));
+        // Initialize last order
+        String lastOrder = "";
+        // Loop through orders.txt file
+        if (!sc.hasNextLine()) {
+            return "Order001";
+        } else {
+            while (sc.hasNextLine()) {
+                String order = sc.nextLine();
+                // Check if the current line is the last line
+                if (!sc.hasNextLine()) {
+                    lastOrder = order;
+                }
+            }
+            // Get order ID
+            String orderID = lastOrder.split(",")[0];
+            // Get number of order ID
+            int idNumber = Integer.parseInt(orderID.substring(5, 8));
+            // Return ID
+            return String.format("Order%03d", idNumber + 1);
+        }
+    }
+    public static void createOrder() throws IOException {
+        // initialize the needed variables
+        Scanner customerInput = new Scanner(System.in);
+        boolean continueToBuy = true;
+        int quantity, itemNum;
+        double chosenItemPrice = 0, total = 0, totalAfterDiscount = 0;
+        HashMap<String, ArrayList<Integer>> orderProducts = new HashMap<>();
+        String chosenProductName = null;
+
+        // loop when customer still want to buy more products
+        while (continueToBuy) {
+            // display all the products
+            System.out.println("PRODUCT");
+            System.out.println("--------------");
+            viewProducts();
+            System.out.println("--------------");
+            System.out.println("Enter the number of the item you want to buy:");
+            itemNum = customerInput.nextInt();
+
+            File productFile = new File("src/products.txt");
+            Scanner productFileScanner = new Scanner(productFile);
+
+            while (productFileScanner.hasNextLine()) {
+                String product = productFileScanner.nextLine();
+                String productId = product.split(",")[0];
+                int idNumber = Integer.parseInt(productId.substring(1, 4));
+                String productName = product.split(",")[1];
+                double productPrice = Double.parseDouble(product.split(",")[2]);
+
+                // when the item number that the user enters equal to the ID number of the product, use that product name and price
+                if (itemNum == idNumber) {
+                    chosenItemPrice = productPrice;
+                    chosenProductName = productName;
+                    break;
+                }
+            }
+
+            System.out.println("How many do you want to buy?");
+            quantity = customerInput.nextInt();
+
+            // update hash map with the product and its quantity.
+            // Use array list in case customers want to add the product that is already in their cart
+
+            if (orderProducts.containsKey(chosenProductName)) {
+                ArrayList<Integer> quantityArrayList;
+                quantityArrayList = orderProducts.get(chosenProductName);
+                quantityArrayList.add(quantity);
+                orderProducts.put(chosenProductName, quantityArrayList);
+            } else {
+                ArrayList<Integer> quantityArrayList = new ArrayList<Integer>();
+                quantityArrayList.add(quantity);
+                orderProducts.put(chosenProductName,quantityArrayList);
+            }
+
+            total = total + (chosenItemPrice * quantity);
+
+            System.out.println("Do you want to continue to buy? (Y or N)");
+            String isContinue = customerInput.next();
+
+            // prevent the case customer enters something apart from Y or N.
+            if (isContinue.equalsIgnoreCase("Y"))
+                System.out.println("-----------------");
+            else if (isContinue.equalsIgnoreCase("N")) {
+                System.out.println("-----------------");
+                continueToBuy = false;
+            }  else
+                System.out.println("Wrong input! Y or N only! Please try again");
+        }
+
+
+        String customerId = "";
+
+        boolean usernameNotFound = true;
+
+        while (usernameNotFound) {
+            // find the customer ID by asking the customer(s) their username
+            System.out.println("Your username is:");
+            String username = customerInput.next();
+
+            File customerFile = new File("src/customers.txt");
+            Scanner customerFileScanner = new Scanner(customerFile);
+
+            while (customerFileScanner.hasNext()) {
+                String customerLine = customerFileScanner.nextLine();
+                String[] customerInfo = customerLine.split(",");
+                if (customerInfo[6].equals(username)) {
+                    customerId = customerInfo[0];
+                    System.out.printf("Thank you %s!\n", username);
+                    String membership = customerInfo[5];
+
+                    // apply membership
+                    if(membership.equals("silver"))
+                        totalAfterDiscount = total * 95/100;
+                    else if (membership.equals("gold"))
+                        totalAfterDiscount = total * 90/100;
+                    else if (membership.equals("platinum"))
+                        totalAfterDiscount = total * 85/100;
+                    else
+                        totalAfterDiscount = total;
+
+                    usernameNotFound = false;
+                } else
+                    System.out.println("Invalid username. Please try again!");
+            }
+        }
+
+
+        // let the customer enter and confirm the shipping address
+        System.out.println("Your shipping address is:");
+        String address = customerInput.next();
+
+        System.out.println("Please re-check your address. Is it correct?(Y or N) :");
+        String addressConfirm = customerInput.next();
+        while (true) {
+            if (addressConfirm.equalsIgnoreCase("Y")) {
+                System.out.println("-----------------");
+                System.out.println("Thank you! You have successfully placed your order");
+                break;
+            }
+
+            if (addressConfirm.equalsIgnoreCase("N")) {
+                System.out.println("Please re-enter your address!");
+                System.out.println("Your shipping address is:");
+                address = customerInput.next();
+            } else {
+                System.out.println("Only enter Y or N");
+            }
+            System.out.println("Please re-check your address. Is it correct? (Y or N) :");
+            addressConfirm = customerInput.next();
+        }
+
+        // generate orderID
+        String orderId = newOrderId();
+
+        // order date
+        String orderDate = LocalDate.now().toString();
+
+
+        // get the list of all products in the order and their quantities
+        Object[] productsList = orderProducts.keySet().toArray();
+        Object[] quantityList = orderProducts.values().toArray();
+
+        // generate strings to store the list of products and their quantities
+        StringBuilder currentOrderProducts = new StringBuilder(productsList[0].toString());
+        StringBuilder currentOrderProductQuantity = new StringBuilder(quantityList[0].toString());
+        for (int i = 1; i < orderProducts.size(); i++) {
+            currentOrderProducts.append(":").append(productsList[i].toString());
+        }
+        for (int i = 1; i < orderProducts.size(); i++) {
+            currentOrderProductQuantity.append(":").append(quantityList[i].toString());
+        }
+
+        // append the line for the new order to the end of the orders.txt file
+        String newOrder = String.join(",", orderId,orderDate,customerId, address,
+                currentOrderProducts.toString(), currentOrderProductQuantity.toString(), String.valueOf(totalAfterDiscount), "delivered");
+        PrintWriter pw = new PrintWriter(new FileWriter("src/orders.txt",true));
+        pw.println(newOrder);
+
+        pw.flush();
+        pw.close();
+
+        // display order details
+        System.out.println("Your order detail");
+        System.out.println("-----------------");
+        System.out.println("Order date: "+ orderDate);
+        System.out.println("Shipping address: "+ address);
+        System.out.println("Items: "+ currentOrderProducts );
+        System.out.println("Quantity: "+ currentOrderProductQuantity);
+        System.out.println("Total: " + total);
+
+        // update membership by adding the order total to total spending
+
     }
 }
 
